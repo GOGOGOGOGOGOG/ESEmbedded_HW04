247a152b5418af0da7bdd3f1d96debfb1972e0ba
JEFF1216 <43406666+GOGOGOGOGOGOG@users.noreply.github.com>
2019-03-24 15:53:43 +0800

    Update blink.h

--- blink.h
+++ blink.h
@@ -1,3 +1,4 @@
+#include "reg.h"
 #ifndef BLINK_H
 #define BLINK_H
 
@@ -13,4 +14,5 @@ void Delay(unsigned int nDelay);
 void close_light(int led);
 void turn_light_all(void);
 void close_light_all (void);
+int GPIO_init (unsigned int GPIO_PORTx , unsigned int GPIO_Pinx , GPIO_InitDef * GPIO_initstruct );
 #endif
