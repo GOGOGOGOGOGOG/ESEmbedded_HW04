9c88d37da3d0f8a611b9ad716e050b43a41158f0
JEFF1216 <43406666+GOGOGOGOGOGOG@users.noreply.github.com>
2019-03-24 15:53:16 +0800

    Update blink.c

--- blink.c
+++ blink.c
@@ -20,6 +20,109 @@ SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(a));
 SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_PUPDR_OFFSET, PUPDRy_0_BIT(a));
 
 }
+
+int GPIO_init (unsigned int GPIO_PORTx , unsigned int GPIO_Pinx , GPIO_InitDef * GPIO_initstruct )
+{
+
+SET_BIT(RCC_BASE + RCC_AHB1ENR_OFFSET, GPIO_EN_BIT(GPIO_PORTx));
+
+if(GPIO_initstruct){
+
+
+  if(GPIO_initstruct->GPIO_Mode == GPIO_Mode_IN )
+  {
+  CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_MODER_OFFSET, MODERy_1_BIT(GPIO_Pinx));
+  CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_MODER_OFFSET, MODERy_0_BIT(GPIO_Pinx));
+  }
+  else if(GPIO_initstruct->GPIO_Mode ==GPIO_Mode_OUT)
+  {
+  CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_MODER_OFFSET, MODERy_1_BIT(GPIO_Pinx));
+  SET_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_MODER_OFFSET, MODERy_0_BIT(GPIO_Pinx));
+  }
+  else if(GPIO_initstruct->GPIO_Mode ==GPIO_Mode_AF)
+  {
+  SET_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_MODER_OFFSET, MODERy_1_BIT(GPIO_Pinx));
+  CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_MODER_OFFSET, MODERy_0_BIT(GPIO_Pinx));
+  }
+  else if(GPIO_initstruct->GPIO_Mode ==GPIO_Mode_AN)
+  {
+  SET_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_MODER_OFFSET, MODERy_1_BIT(GPIO_Pinx));
+  SET_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_MODER_OFFSET, MODERy_0_BIT(GPIO_Pinx));
+  }
+  else if (GPIO_initstruct->GPIO_Mode == 0)
+  {
+	   return GPIO_SET_fail;
+ }
+/* GPIO_Type_def */
+
+  if(GPIO_initstruct->GPIO_OType == GPIO_Type_pp ) 
+  {
+  CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_MODER_OFFSET, OTy_BIT(GPIO_Pinx));
+  }
+  else if(GPIO_initstruct->GPIO_OType==GPIO_Type_OD)
+  {
+  SET_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_MODER_OFFSET, OTy_BIT(GPIO_Pinx));
+  }
+ else if (GPIO_initstruct->GPIO_OType==0)
+  {
+  return GPIO_SET_fail;
+  }
+ /* GPIO_SPEED_def */
+
+ if(GPIO_initstruct->GPIO_Speed ==GPIO_High_speed)
+ {
+  SET_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_1_BIT(GPIO_Pinx));
+  SET_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_0_BIT(GPIO_Pinx));
+ }
+ else if(GPIO_initstruct->GPIO_Speed ==GPIO_Fast_speed)
+ { 
+  SET_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_1_BIT(GPIO_Pinx));
+  CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_0_BIT(GPIO_Pinx));
+ }
+ else if(GPIO_initstruct->GPIO_Speed ==GPIO_Medium_speed)
+ {
+  CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_1_BIT(GPIO_Pinx));
+  SET_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_0_BIT(GPIO_Pinx));
+ }
+  else if(GPIO_initstruct->GPIO_Speed ==GPIO_Low_speed)
+ {
+  CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_1_BIT(GPIO_Pinx));
+  CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_0_BIT(GPIO_Pinx));
+ }
+  else if (GPIO_initstruct->GPIO_Speed ==0)
+  {
+
+   return GPIO_SET_fail;
+}
+
+/*GPIO_PUPD_def*/
+if(GPIO_initstruct->GPIO_PuPd==GPIO_PuPd_nopull)
+{
+//PUPDR led pin = 01 => no pull-up,NO pull-down
+CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(GPIO_Pinx));
+CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_PUPDR_OFFSET, PUPDRy_0_BIT(GPIO_Pinx));
+}
+
+else if(GPIO_initstruct->GPIO_PuPd ==GPIO_PuPd_up)
+{
+CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(GPIO_Pinx));
+SET_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(GPIO_Pinx));
+
+}
+else if(GPIO_initstruct->GPIO_PuPd ==GPIO_PuPd_down)
+{
+SET_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(GPIO_Pinx));
+CLEAR_BIT(GPIO_BASE(GPIO_PORTx) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(GPIO_Pinx));
+}
+else if(GPIO_initstruct->GPIO_PuPd==0)
+{
+return GPIO_SET_fail;
+}
+
+}
+return GPIO_SET_READY;
+}
+
 void user_button_init(void)
 {
 	//int a=0;
@@ -113,9 +216,9 @@ void blink_count (unsigned int led,int count)
 }
 void close_light(int led)
 {
-unsigned int i;
+//unsigned int i;
 	 SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BRy_BIT(led));
-	 for (i = 0; i < 100000; i++);
+	Delay(50000);
 }
 void turn_light_all (void)
 {
@@ -125,7 +228,7 @@ SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BSy_BIT((int) ((led>>12&0x0C)
 SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BSy_BIT((int)((led>>8&0x0D))));
 SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BSy_BIT((int)((led>>4&0x0E))));
 SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BSy_BIT((int)((led&0x0F))));
-for (i = 0; i < 1000000; i++) 
+   for (i = 0; i < 1000000; i++) 
 			;
 }
 void close_light_all(void)
@@ -136,6 +239,6 @@ SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BRy_BIT((int) ((led>>12&0x0C)
 SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BRy_BIT((int)((led>>8&0x0D))));
 SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BRy_BIT((int)((led>>4&0x0E))));
 SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BRy_BIT((int)((led&0x0F))));
-for (i = 0; i < 1000000; i++) 
+ for (i = 0; i < 1000000; i++) 
 			;
 }
